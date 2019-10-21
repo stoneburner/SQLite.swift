d60e5330de127cebf529d846fc7b0dfab815e656
Alexander Kasimir <alexander@kasimir.at>
2019-10-21 15:27:48 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
-    products: [.library(name: "SQLite", targets: ["SQLite"])],
-    package.dependencies = [.package(url: "https://github.com/stoneburner/SQLCipher")],
+    products: [.library(name: "SQLite", targets: ["SQLite"])],    
+    dependencies: [
+        .package(url: "https://github.com/stoneburner/SQLCipher", from: "master"),
+    ],
     targets: [
         .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
         .target(name: "SQLiteObjc"),
