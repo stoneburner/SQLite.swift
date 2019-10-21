3bbe450f635dc4fb35c3398bc8a7d64d3d074faf
Alexander Kasimir <alexander@kasimir.at>
2019-10-21 15:19:42 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SQLite.swift",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
+    package.dependencies = [.package(url: "https://github.com/stoneburner/SQLCipher"]
     targets: [
         .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
         .target(name: "SQLiteObjc"),
@@ -11,14 +12,3 @@ let package = Package(
     ],
     swiftLanguageVersions: [4, 5]
 )
-
-#if os(Linux)
-    package.dependencies = [.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")]
-    package.targets = [
-        .target(name: "SQLite", exclude: ["Extensions/FTS4.swift", "Extensions/FTS5.swift"]),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests", exclude: [
-            "FTS4Tests.swift",
-            "FTS5Tests.swift"
-        ])
-    ]
-#endif
